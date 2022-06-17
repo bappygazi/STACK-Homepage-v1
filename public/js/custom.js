c11857f1c12df44e6dbc7785f7874c84a2226d14
Bappy Gazi <41175073+bappygazi@users.noreply.github.com>
2022-06-17 12:12:10 +0600

    sdfdfsa

---
+++ public/js/custom.js
@@ -0,0 +1,117 @@
+/* JS */
+$(function () {
+    //    sticky navbar
+    $(window).scroll(function () {
+        var scrolling = $(this).scrollTop();
+        var sticky = $(".navbar");
+        if (scrolling >= 350) {
+            sticky.addClass("navbg");
+        } else {
+            sticky.removeClass("navbg");
+        }
+    });
+
+    //    parallax logo
+
+    $(window).scroll(function () {
+        var scrolling = $(this).scrollTop();
+        var sticky = $(".navbar");
+        if (scrolling >= 100) {
+            sticky.addClass("parallax__logo");
+        } else {
+            sticky.removeClass("parallax__logo");
+        }
+    });
+
+
+    //  workplace owl-carouse
+    $('#hero .owl-carousel').owlCarousel({
+        center: true,
+        loop: true,
+        nav: true,
+        autoplay: true,
+        smartSpeed: 5000,
+        animateIn: "fadeIn",
+        animateOut: "fadeOut",
+        navText: [" ", "<i class='fal fa-long-arrow-right'></i>"],
+        responsive: {
+            0: {
+                nav: false,
+                items: 1
+
+            },
+            750: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+
+    $('#portfolio .owl-carousel').owlCarousel({
+        loop: true,
+        margin: 30,
+        nav: true,
+        dots: false,
+        smartSpeed: 1250,
+        navText: [" <i class='fal fa-long-arrow-left'> ", "<i class='fal fa-long-arrow-right'>"],
+        responsive: {
+            0: {
+                nav: false,
+                margin: 15,
+                items: 1.1
+            },
+            750: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            },
+        }
+    })
+    //  services owl-carouse
+    $('#services .owl-carousel').owlCarousel({
+        loop: true,
+        margin: 30,
+        nav: true,
+        dots: false,
+        smartSpeed: 1200,
+        navText: [" <i class='fal fa-long-arrow-left'></i> ", "<i class='fal fa-long-arrow-right'></i>"],
+        responsive: {
+            0: {
+                nav: false,
+                margin: 15,
+                items: 1.1
+            },
+            750: {
+                items: 3
+            },
+            1920: {
+                items: 3
+            }
+        }
+    })
+    //  testimonial owl-carouse
+    $('#testimonial .owl-carousel').owlCarousel({
+        loop: true,
+        margin: 30,
+        nav: true,
+        dots: false,
+        smartSpeed: 1200,
+        navText: [" ", "<i class='fal fa-long-arrow-right'></i>"],
+        responsive: {
+            0: {
+                nav: false,
+                dots: true,
+                items: 1
+            },
+            750: {
+                items: 1
+            },
+            1920: {
+                items: 1
+            }
+        }
+    });
+});
